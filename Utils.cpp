7aa74577973ae2ddee0ac6a6ccc431a4dc5f5c31
MateuszKucio <mateusz.kucio@vp.pl>
2024-12-31 13:17:54 +0100

    Usprawniono działanie menu, dodano nowe przyciski, sekcje Achieveme (#21)
    nts i Statystyki. Dodano funkcję do centrowania tekstu i plik utils na funkcje pomocnicze.
    
    Co-authored-by: MateuszKucio <mateusz@AsusS14X>
    Co-authored-by: Krzychu2567 <apollokk@outlook.com>

---
+++ Utils.cpp
@@ -0,0 +1,30 @@
+#include "Utils.h"
+
+// Funkcja do wyśrodkowania tekstu w oknie o podanych wymiarach
+void centerText(sf::Text& text, float windowWidth, float windowHeight, float verticalPosition) {
+    // Obliczanie szerokości tekstu
+    sf::FloatRect textBounds = text.getLocalBounds();
+    
+    // Ustawienie originu na środek szerokości tekstu
+    text.setOrigin(
+        textBounds.left + textBounds.width / 2,  // Środek szerokości tekstu
+        textBounds.top                            // Górna krawędź tekstu
+    );
+    
+    // Ustawienie pozycji tekstu na środku okna poziomo oraz wybranej pozycji pionowo
+    text.setPosition(windowWidth / 2, verticalPosition);
+}
+
+void drawScrollableList(sf::RenderWindow& window, const std::vector<std::string>& items, sf::Font& font) {
+    sf::View view = window.getDefaultView();
+    view.setViewport(sf::FloatRect(0, 0, 1, 1));
+    window.setView(view);
+
+    float yOffset = 0;
+    for (const auto& item : items) {
+        sf::Text text(item, font, 20);
+        text.setPosition(50, 50 + yOffset);
+        window.draw(text);
+        yOffset += 30;
+    }
+}
---
+++ Utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <SFML/Graphics.hpp>
+
+// Deklaracja funkcji do wyśrodkowania tekstu
+void centerText(sf::Text& text, float windowWidth, float windowHeight, float verticalPosition);
+// Funkcja do listy przewijanej
+void drawScrollableList(sf::RenderWindow& window, const std::vector<std::string>& items, sf::Font& font);
+
+#endif // UTILS_H
