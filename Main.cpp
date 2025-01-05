4f455f4914f6b0fe1378bdddedbde09a5903b437
MateuszKucio <mateusz.kucio@vp.pl>
2025-01-05 17:27:08 +0100

    Wstawienie tekstur (#24)
    * Usprawniono działanie menu, dodano nowe przyciski, sekcje Achieveme
    nts i Statystyki. Dodano funkcję do centrowania tekstu i plik utils na funkcje pomocnicze.
    
    * Obsługa polskich znaków.
    
    * Wstawiono tekstury
    
    ---------
    
    Co-authored-by: MateuszKucio <mateusz@AsusS14X>
    Co-authored-by: Krzychu2567 <apollokk@outlook.com>

--- Main.cpp
+++ Main.cpp
@@ -16,32 +16,66 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1200, 640), "Dino Game");
     window.setFramerateLimit(60); // Ograniczenie FPS do 60
 
+    // Załaduj teksturę tła
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("Tekstury/menu.png")) {
+        std::cerr << L"Nie udało się załadować tekstury tła!" << std::endl;
+        return -1;
+    }
+
+    // Utwórz sprite z tekstury tła
+    sf::Sprite backgroundSprite;
+    backgroundSprite.setTexture(backgroundTexture);
+
+    // Oblicz skalę dla tła menu
+    float scaleX = static_cast<float>(window.getSize().x) / backgroundTexture.getSize().x;
+    float scaleY = static_cast<float>(window.getSize().y) / backgroundTexture.getSize().y;
+    backgroundSprite.setScale(scaleX, scaleY);
+
+    // Załaduj teksturę tła trybu fabularnego
+    sf::Texture storyBackgroundTexture;
+    if (!storyBackgroundTexture.loadFromFile("Tekstury/background-dino.png")) {
+        std::cerr << L"Nie udało się załadować tekstury tła trybu fabularnego!" << std::endl;
+        return -1;
+    }
+
+    // Utwórz sprite z tekstury tła trybu fabularnego
+    sf::Sprite storyBackgroundSprite;
+    storyBackgroundSprite.setTexture(storyBackgroundTexture);
+
+    // Oblicz skalę dla tła trybu fabularnego
+    float storyScaleX = static_cast<float>(window.getSize().x) / storyBackgroundTexture.getSize().x;
+    float storyScaleY = static_cast<float>(window.getSize().y) / storyBackgroundTexture.getSize().y;
+    storyBackgroundSprite.setScale(storyScaleX, storyScaleY);
+
+
+    // Sprawdzenie czy udało się załadować czcionkę
     sf::Font font;
     if (!font.loadFromFile("arial.ttf")) {
-        std::cerr << "Nie udało się załadować czcionki!" << std::endl;
+        std::cerr << L"Nie udało się załadować czcionki!" << std::endl;
         return -1;
     }
 
     // Tworzenie przycisków
-    Button storyButton(sf::Vector2f(324, 54), sf::Vector2f(438, 260), "Tryb fabularny", font);
-    storyButton.setTexture("Tekstury/ustawienia.png");
-    Button endlessButton(sf::Vector2f(324, 54), sf::Vector2f(438, 352), "Endless", font);
-    endlessButton.setTexture("Tekstury/ustawienia.png");
-    Button optionsButton(sf::Vector2f(54, 54), sf::Vector2f(300, 486), "Opcje", font);
-    optionsButton.setTexture("Tekstury/ustawienia.png");
-    Button achievementsButton(sf::Vector2f(54, 54), sf::Vector2f(482, 486), "Osiągnięcia", font);
-    achievementsButton.setTexture("Tekstury/ustawienia.png");
-    Button statisticsButton(sf::Vector2f(54, 54), sf::Vector2f(664, 486), "Statystyki", font);
-    statisticsButton.setTexture("Tekstury/ustawienia.png");
-    Button backButton(sf::Vector2f(200, 50), sf::Vector2f(500, 400), "Powrót", font);
-    Button exitButton(sf::Vector2f(54, 54), sf::Vector2f(846, 486), "Wyjście", font);
-    exitButton.setTexture("Tekstury/ustawienia.png");
+    Button storyButton(sf::Vector2f(324, 54), sf::Vector2f(250, 372), " ", font);
+    storyButton.setTexture("Tekstury/przyciskSTART.png");
+    Button endlessButton(sf::Vector2f(324, 54), sf::Vector2f(626, 372), " ", font);
+    endlessButton.setTexture("Tekstury/przyciskENDLESS.png");
+    Button optionsButton(sf::Vector2f(54, 54), sf::Vector2f(300, 486), " ", font);
+    optionsButton.setTexture("Tekstury/settings.png");
+    Button achievementsButton(sf::Vector2f(54, 54), sf::Vector2f(482, 486), " ", font);
+    achievementsButton.setTexture("Tekstury/archievment.png");
+    Button statisticsButton(sf::Vector2f(54, 54), sf::Vector2f(664, 486), " ", font);
+    statisticsButton.setTexture("Tekstury/statistic.png");
+    Button backButton(sf::Vector2f(200, 50), sf::Vector2f(500, 400), L"Powrót", font);
+    Button exitButton(sf::Vector2f(54, 54), sf::Vector2f(846, 486), " ", font);
+    exitButton.setTexture("Tekstury/exit.png");
     Button pauseButton(sf::Vector2f(200, 50), sf::Vector2f(10, 10), "Pause", font);
     //pauseButton.setTexture("Tekstury/.png");
-    Button resumeButton(sf::Vector2f(200, 50), sf::Vector2f(10, 10), "Wznów", font);
+    Button resumeButton(sf::Vector2f(200, 50), sf::Vector2f(10, 10), L"Wznów", font);
     //resumeButton.setTexture("Tekstury/.png");
     Button restartButton(sf::Vector2f(200, 50), sf::Vector2f(500, 320), "Restart", font);
-    Button mainMenuButton(sf::Vector2f(200, 50), sf::Vector2f(500, 390), "Menu Główne", font);
+    Button mainMenuButton(sf::Vector2f(200, 50), sf::Vector2f(500, 390), L"Menu Główne", font);
 
     // Tworzenie gracza
     Player player(sf::Vector2f(50.f, 80.f), sf::Vector2f(100.f, 500.f), sf::Color::Cyan);
@@ -59,7 +93,7 @@ int main() {
     std::vector<std::string> statistics = {"Statistic 1", "Statistic 2", "Statistic 3"};
 
     // Tworzenie tekstu
-    sf::Text nameText("Dino Game", font, 70);
+    sf::Text nameText(" ", font, 70);
 
     // Wyśrodkowanie tekstu
     centerText(nameText, 1200, 640, 60);  // Wyśrodkowanie w poziomie, wysokość 60
@@ -214,10 +248,10 @@ int main() {
             coinManager.update(deltaTime, player.getGlobalBounds(), coinCount, obstacleManager.getObstacleBounds()); // Aktualizacja monet
 
             distance += 0.05f;
-            distanceText.setString("Odleglosc: " + std::to_string(static_cast<int>(distance)));
-            coinCountText.setString("Monety: " + std::to_string(coinCount));
-
-	    
+          
+            distanceText.setString(L"Odleglosc: " + std::to_wstring(static_cast<int>(distance)));
+            coinCountText.setString(L"Monety: " + std::to_wstring(coinCount));
+          
         // Sprawdzenie kolizji i koniec gry lub restart
             if (obstacleManager.checkCollisions(player.getGlobalBounds())) {
                 gameState = GameOver;
@@ -251,6 +285,7 @@ int main() {
         window.clear(sf::Color::Black);
 
         if (gameState == MainMenu) {
+            window.draw(backgroundSprite); // Rysowanie tła
             storyButton.draw(window);
             endlessButton.draw(window);
             optionsButton.draw(window);
@@ -268,6 +303,7 @@ int main() {
             drawScrollableList(window, statistics, font);
             backButton.draw(window);
         } else if (gameState == Gameplay) {
+            window.draw(storyBackgroundSprite); // Rysowanie tła trybu fabularnego
             player.draw(window); // Rysowanie gracza
 	        obstacleManager.draw(window);  // Rysowanie przeszkód
             coinManager.draw(window);
@@ -282,7 +318,7 @@ int main() {
             mainMenuButton.draw(window);
         } else if (gameState == GameOver) {
             window.clear(sf::Color::Black);
-            sf::Text gameOverText("Sprobuj jeszcze raz", font, 50);
+            sf::Text gameOverText(L"Spróbuj jeszcze raz", font, 50);
             gameOverText.setPosition(400, 200);
             window.draw(gameOverText);
             restartButton.draw(window);
