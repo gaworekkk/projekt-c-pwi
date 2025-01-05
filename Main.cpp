4df4dbf4ccaeda8543b3e313615c6a1b99bf7ec0
Krzychu2567 <apollokk@outlook.com>
2025-01-05 21:14:34 +0100

    Poprawione zliczanie odległości

--- Main.cpp
+++ Main.cpp
@@ -251,14 +251,8 @@ int main() {
 	        obstacleManager.update(deltaTime);  // Aktualizacja przeszkód
             coinManager.update(deltaTime, player.getGlobalBounds(), coinCount, obstacleManager.getObstacleBounds()); // Aktualizacja monet
 
-            float playerSpeed = player.getVelocity().x;
-            if(playerSpeed > 0) {
-                distance += 0.1f;
-            } else if (playerSpeed < 0) {
-                distance += 0.025f;
-            } else {
-                distance += 0.05f;
-            }
+            float playerVelocity = player.getVelocity().x;
+            distance += (deltaTime * (obstacleManager.getSpeed() + playerVelocity)) / 250;
           
             distanceText.setString(L"Odleglosc: " + std::to_wstring(static_cast<int>(distance)));
             coinCountText.setString(L"Monety: " + std::to_wstring(coinCount));
