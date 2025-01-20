6f33d8784cdbb957ba4c3dabc78ab8878d2cf765
MateuszKucio <mateusz.kucio@vp.pl>
2025-01-20 21:21:24 +0100

    Wyciagnięcie Dino z nieskończonego portalu (#84)
    * Naprawa gifów (bez poziomu latajacego)
    
    * Naprawa gifów (bugfix)
    
    * Usuinieto zone identifajery
    
    ---------
    
    Co-authored-by: MateuszKucio <mateusz@AsusS14X>

--- ResourceLoader.cpp
+++ ResourceLoader.cpp
@@ -21,7 +21,7 @@ sf::Sprite shopBackgroundSprite;
 // Dino
 sf::Texture dinoTexture;
 sf::Sprite dinoSprite;
-std::string dinoTexturePath[6] = {"Tekstury/skórki dino/dino2(run).gif", "Tekstury/skórki dino/dino 4 (klatki)/dino_sprite_sheet.png", "Tekstury/skórki dino/dino6(run).gif", "Tekstury/skórki dino/dino6(run).gif", "Tekstury/skórki dino/dino7(run).gif", "Tekstury/skórki dino/dino8(run).gif"};
+std::string dinoTexturePath[6] = {"Tekstury/skórki dino/dino2_sprite_sheet.png", "Tekstury/skórki dino/dino 4 (klatki)/dino_sprite_sheet.png", "Tekstury/skórki dino/dino5_sprite_sheet.png", "Tekstury/skórki dino/dino6run_spritesheet.png", "Tekstury/skórki dino/dino7run_spritesheet.png", "Tekstury/skórki dino/dino8run_spritesheet.png"};
 
 // Czcionka
 sf::Font font;
